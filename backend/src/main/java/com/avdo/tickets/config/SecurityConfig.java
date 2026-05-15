c9101da7a9720df0e91524b9da38e39f5f7b88f7
abdallaaraby19 <abdallamahmoud642@gmail.com>
2026-05-15 17:31:15 +0300

    Add JWT authentication support and QR code generation configuration

--- backend/src/main/java/com/avdo/tickets/config/SecurityConfig.java
+++ backend/src/main/java/com/avdo/tickets/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.avdo.tickets.config;
 import com.avdo.tickets.filters.UserProvisioningFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -12,17 +13,29 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfig {
 
-    @Bean
-    public SecurityFilterChain filterChain(
-            HttpSecurity httpSecurity,
-            UserProvisioningFilter userProvisioningFilter){
-        httpSecurity.authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
-                .csrf(csrf -> csrf.disable())
-                .sessionManagement(session ->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .oauth2ResourceServer(oauth2 ->oauth2.jwt(Customizer.withDefaults()))
-                .addFilterAfter(userProvisioningFilter, BearerTokenAuthenticationFilter.class);
-        return httpSecurity.build();
+  @Bean
+  public SecurityFilterChain filterChain(
+      HttpSecurity http,
+      UserProvisioningFilter userProvisioningFilter,
+      JwtAuthenticationConverter jwtAuthenticationConverter) throws Exception {
+    http
+        .authorizeHttpRequests(authorize ->
+            authorize
+                .requestMatchers(HttpMethod.GET, "/api/v1/published-events/**").permitAll()
+                .requestMatchers("/api/v1/events").hasRole("ORGANIZER")
+                // Catch all rule
+                .anyRequest().authenticated())
+        .csrf(csrf -> csrf.disable())
+        .sessionManagement(session ->
+            session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .oauth2ResourceServer(oauth2 ->
+            oauth2.jwt(jwt ->
+                jwt.jwtAuthenticationConverter(jwtAuthenticationConverter)
+            ))
+        .addFilterAfter(userProvisioningFilter, BearerTokenAuthenticationFilter.class);
 
-    }
+    return http.build();
+  }
 
 }
+
